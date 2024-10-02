0d998c7d2ed93075d46cff0484a3a83655ca41bc
Achriki <adam.chkpr@gmail.com>
2024-10-02 18:54:40 +0100

    Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import dbClient from "../utils/db";
+import redisClient from "../utils/redis";
 import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 
@@ -30,6 +31,25 @@ class UsersController {
         }catch(error){
             console.log(error)
             response.status(500).json({ error: 'Server error' });
+        }
+    }
+
+    static async getMe (request, response) {
+        try {
+          const userToken = request.header('X-Token');
+          const authKey = `auth_${userToken}`;
+          // console.log('USER TOKEN GET ME', userToken);
+          const userID = await redisClient.get(authKey);
+          console.log('USER KEY GET ME', userID);
+          if (!userID) {
+            response.status(401).json({ error: 'Unauthorized' });
+          }
+          const user = await dbClient.getUser({ _id: ObjectId(userID) });
+          // console.log('USER GET ME', user);
+          response.json({ id: user._id, email: user.email });
+        } catch (error) {
+          console.log(error);
+          response.status(500).json({ error: 'Server error' });
         }
     }
 }
